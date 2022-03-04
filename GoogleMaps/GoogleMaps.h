230a58f239c4c87f15997c7eabfe65660b2d08aa
David Kadlček <david.kadlcek@matee.cz>
2022-03-04 17:36:27 +0100

    Created package

---
+++ GoogleMaps/GoogleMaps.h
@@ -0,0 +1,17 @@
+//
+//  GoogleMaps.h
+//  GoogleMaps
+//
+//  Created by David Kadlček on 04.03.2022.
+//
+
+
+//! Project version number for GoogleMaps.
+FOUNDATION_EXPORT double GoogleMapsVersionNumber;
+
+//! Project version string for GoogleMaps.
+FOUNDATION_EXPORT const unsigned char GoogleMapsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleMaps/PublicHeader.h>
+
+
