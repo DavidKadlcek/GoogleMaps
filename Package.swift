5ce583d62b7236b255440df706f602af942e78da
David Kadlček <david.kadlcek@matee.cz>
2022-03-04 16:54:56 +0100

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,48 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "GoogleMaps",
+    products: [
+        .library(
+            name: "GoogleMapsBase",
+            targets: [
+                "GoogleMapsBase"
+            ]
+        ),
+        .library(
+            name: "GoogleMapsCore",
+            targets: [
+                "GoogleMapsCore"
+            ]
+        ),
+        .library(
+            name: "GoogleMaps",
+            targets: [
+                "GoogleMaps",
+                "GoogleMapsBase",
+                "GoogleMapsCore"
+            ]
+        ),
+        .library(
+            name: "GoogleMapsM4B",
+            targets: [
+                "GoogleMapsM4B"
+            ]
+        ),
+        .library(
+            name: "GooglePlaces",
+            targets: [
+                "GooglePlaces",
+                "GoogleMapsBase"
+            ]
+        )
+    ],
+    targets: [
+        .binaryTarget(name: "GoogleMaps", path: "./GoogleMaps.xcframework"),
+        .binaryTarget(name: "GoogleMapsCore", path: "./GoogleMapsCore.xcframework"),
+        .binaryTarget(name: "GoogleMapsBase", path: "./GoogleMapsBase.xcframework"),
+        .binaryTarget(name: "GoogleMapsM4B", path: "./GoogleMapsM4B.xcframework"),
+        .binaryTarget(name: "GooglePlaces", path: "./GooglePlaces.xcframework")
+    ]
+)
